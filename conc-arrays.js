bde6aeb94a77e86261bd7ea7d8b0e3d9a8e17805
Luiza <luizaxwm@gmail.com>
2022-06-10 11:51:06 -0300

    JS Basics.

---
+++ conc-arrays.js
@@ -0,0 +1,6 @@
+const room1 = [' Luíza', ' Rodrigo']
+const room2 = [' Lígia', ' João']
+// const room3 = [' Lucas', ' Miguel']
+
+const newRoom = room1.concat(room2)
+console.log(newRoom)
---
+++ matriz.js
@@ -0,0 +1,6 @@
+const alunos = [' Luíza', ' Rodrigo', ' Lígia', ' João']
+const mediasDosAlunos = [10, 9, 8, 7]
+
+let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
+
+console.log(`${listaDeNotasEAlunos[0][0]}, a sua média é ${listaDeNotasEAlunos[1][0]}`)
